77da3ecd7d5519ce3e7cc8d598e0fb7b8c2ab03b
Michael Dippery <michael@monkey-robot.com>
2016-06-22 22:04:03 -0700

    First draft of playground file

--- Optionals.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Optionals.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -10,49 +10,90 @@
 
  */
 /*: question1
- ### 1. Question here
+ ### 1. What is the type of the variable `pet` declared below?
  */
-// write your code here
+var pet = "turtle 🐢"
 
 
 
 
 /*: question2
- ### 2. Other question.
+ ### 2. What is the type of the variable `petName` declared below?
  */
-// write your code here
+var petName: String?
 
 
 
 
 /*: question3
- ### 3. Other question.
+ ### 3. Currently, what is the _value_ of `petName`?
  */
-// write your code here
 
 
 
 
 
 /*: question4
- ### 4. Questione
+ ### 4. Give `petName` a value! Assign to it any string. What is its type now?
  */
-
-
-
-
-
+// write your code here
 
 
 
 
 /*: question5
- ### 5. Another one
+ ### 5. Print out `petName` using Swift's `print()` function. What do you expect to see in the console?
  */
+// write your code here
 
 
 
 
+/*: question6
+ ### 6. Write an if statement that only prints `petName`'s value if it is not `nil`. If `petName` is `nil`, print, "There is no pet name." What do you expect to see in the console?
+ */
+// write your code here
+
+
+
+
+/*: question7
+ ### 7. Use _optional binding_ to unwrap the value of `petName` and print it to the console without all the **Optional(...)** stuff around it.
+ */
+// write your code here
+
+
+
+
+/*: question8
+ ### 8. Write an if statement that will print out "The value of anotherPetName is nil" using the variable below
+ */
+var anotherPetName: String?
+// write your code here
+
+
+
+
+/*: question9
+ ### 9. Using optional binding, write an if statement that prints "anotherPetName has no value" if it is `nil`; otherwise, print the _unwrapped_ value of `anotherPetName`. What do you expect to see in the console?
+ */
+// write your code here
+
+
+
+
+/*: question10
+ ### 10. Declare a _constant_ optional string (using the `let` keyword). Then try to assign a string value to it on the next line. Print out the constant. What happens?
+ */
+// write your code here
+
+
+
+
+/*: question11
+ ### 11. Declare another _constant_ optional string, and set it to `nil` immediately. Then, on the next line, assign a string value to it. What happens?
+ */
+// write your code here
 
 
 
